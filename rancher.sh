e39413843b6dccbe96d4a67dae5c932ba71e8422
moshin34 <145878819+moshin34@users.noreply.github.com>
2025-01-10 21:57:04 -0500

    Update rancher.sh

--- rancher.sh
+++ rancher.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+brew install kubectl
+brew install helm
+
+
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 kubectl create namespace cattle-system
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.crds.yaml
+
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm install cert-manager jetstack/cert-manager \
@@ -9,8 +14,8 @@ helm install cert-manager jetstack/cert-manager \
   --create-namespace
 helm install rancher rancher-latest/rancher \
   --namespace cattle-system \
-  --set hostname=cirrus.thehussains.org \
+  --set hostname=stratus \
   --set replicas=1 \
-  --set bootstrapPassword=Chibears2025!!
+  --set bootstrapPassword=Welcome25!
 
   echo "Done and done!  Now DO YOUR LONGHORN CHECK"
