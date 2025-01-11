3fbce5ac5ab119bbb6f44c0b191005a0022dc14f
moshin34 <145878819+moshin34@users.noreply.github.com>
2025-01-10 21:38:25 -0500

    Update initnode.sh

--- initnode.sh
+++ initnode.sh
@@ -1,17 +1,8 @@
 
 #!/bin/bash
-
 # Get the IP address using hostname
 host_ip=$(hostname -I | awk '{print $1}')
-
-
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.4+k3s1 sh -s - server --cluster-init
-
-
-
-
-
 echo 
-
 "K3S is now installed.   Now you'll need to copy the KubeConifig to your local workstation.  
 From the workstation use: scp root@$host_ip:/etc/rancher/k3s/k3s.yaml ~/.kube/config"
